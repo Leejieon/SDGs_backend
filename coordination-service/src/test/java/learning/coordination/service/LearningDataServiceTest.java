a2309e454176ad596149bac55f44b605eefa2430
kalteon <102862962+kalteon@users.noreply.github.com>
2023-08-24 16:43:31 +0900

    test

---
+++ coordination-service/src/test/java/learning/coordination/service/LearningDataServiceTest.java
@@ -0,0 +1,75 @@
+package learning.coordination.service;
+
+import learning.coordination.dto.learning_data.CompletedLearningData;
+import learning.coordination.service.default_values.LearningDataDefaultValues;
+import learning.coordination.service.test_values.LearningDataTestValues;
+import learning.coordination.service.test_values.LearningInformationTestValues;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class LearningDataServiceTest {
+
+    @Autowired
+    private LearningDataService learningDataService;
+
+    @BeforeEach
+    public void initQuestion() {
+        // given
+        learningDataService.initLearningData(LearningDataTestValues.TEST_ID);
+    }
+
+    @Test
+    public void shouldFindAnswer() {
+        // when
+        String expectedAnswer = LearningDataDefaultValues.EMPTY_ANSWER;
+        String resultAnswer = learningDataService.findAnswer(LearningDataTestValues.TEST_ID);
+        //given
+        Assertions.assertThat(resultAnswer).isEqualTo(expectedAnswer);
+    }
+
+    @Test
+    public void shouldUpdateAnswer() {
+        // when
+        learningDataService.updateAnswer(LearningDataTestValues.TEST_ID, LearningDataTestValues.TEST_ANSWER);
+        String resultUpdatedAnswer = learningDataService.findAnswer(LearningDataTestValues.TEST_ID);
+        //then
+        Assertions.assertThat(resultUpdatedAnswer).isEqualTo(LearningDataTestValues.TEST_ANSWER);
+    }
+
+    @Test
+    public void shouldSetLearningTarget() {
+        // when
+        learningDataService.setLearningTarget(LearningDataTestValues.TEST_ID, LearningDataTestValues.TEST_LEARNING_TARGET);
+        String resultLearningTarget = learningDataService.getCompletedLearningData(LearningDataTestValues.TEST_ID).getLearningTarget();
+        // then
+        Assertions.assertThat(resultLearningTarget).isEqualTo(LearningDataTestValues.TEST_LEARNING_TARGET);
+    }
+
+    @Test
+    public void shouldSetMaterial() {
+        // when
+        learningDataService.setMaterial(1L, LearningDataTestValues.TEST_MATERIAL);
+        String resultMaterial = learningDataService.getCompletedLearningData(LearningDataTestValues.TEST_ID).getMaterial();
+        // then
+        Assertions.assertThat(resultMaterial).isEqualTo(LearningDataTestValues.TEST_MATERIAL);
+    }
+
+    @Test
+    public void shouldGetCompletedLearningData() {
+        // when
+        learningDataService.updateAnswer(LearningDataTestValues.TEST_ID, LearningDataTestValues.TEST_ANSWER);
+        learningDataService.setLearningTarget(LearningDataTestValues.TEST_ID, LearningDataTestValues.TEST_LEARNING_TARGET);
+        learningDataService.setMaterial(LearningDataTestValues.TEST_ID, LearningDataTestValues.TEST_MATERIAL);
+        CompletedLearningData resultCompletedLearningData = learningDataService.getCompletedLearningData(LearningDataTestValues.TEST_ID);
+        // then
+        Assertions.assertThat(resultCompletedLearningData.getModifiedAnswer()).isEqualTo(LearningDataTestValues.TEST_ANSWER);
+        Assertions.assertThat(resultCompletedLearningData.getLearningTarget()).isEqualTo(LearningDataTestValues.TEST_LEARNING_TARGET);
+        Assertions.assertThat(resultCompletedLearningData.getMaterial()).isEqualTo(LearningDataTestValues.TEST_MATERIAL);
+    }
+}
---
+++ coordination-service/src/test/java/learning/coordination/service/QuestionServiceTest.java
@@ -0,0 +1,85 @@
+package learning.coordination.service;
+
+import learning.coordination.dto.question.EnglishKeywords;
+import learning.coordination.dto.question.SelectedEnglishKeywords;
+import learning.coordination.service.default_values.QuestionDefaultValues;
+import learning.coordination.service.test_values.QuestionTestValues;
+import org.assertj.core.api.Assertions;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class QuestionServiceTest {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @BeforeEach
+    public void initQuestion() {
+        // given
+        questionService.initQuestion(QuestionTestValues.TEST_ID);
+    }
+
+    @Test
+    public void shouldFindTemplateById() {
+        // when
+        String resultTemplate = questionService.findTemplateById(QuestionTestValues.TEST_ID);
+        // then
+        Assertions.assertThat(resultTemplate).isEqualTo(QuestionDefaultValues.TEMPLATE);
+    }
+
+    @Test
+    public void shouldFindEnglishKeywordsById() {
+        // when
+        EnglishKeywords resultEnglishKeywords = questionService.findEnglishKeywordsById(QuestionTestValues.TEST_ID);
+        // then
+        Assertions.assertThat(resultEnglishKeywords.getDifficulties()).containsExactlyElementsOf(QuestionDefaultValues.DIFFICULTIES);
+        Assertions.assertThat(resultEnglishKeywords.getFields()).containsExactlyElementsOf(QuestionDefaultValues.FIELDS);
+        Assertions.assertThat(resultEnglishKeywords.getTopics()).containsExactlyElementsOf(QuestionDefaultValues.TOPICS);
+    }
+
+    @Test
+    public void shouldFindPromptById() {
+        // when
+        String resultPrompt = questionService.findPromptById(QuestionTestValues.TEST_ID);
+        // then
+        Assertions.assertThat(resultPrompt).isEqualTo(QuestionDefaultValues.EMPTY_PROMPT);
+    }
+    @Test
+    public void shouldUpdatePromptById() {
+        // when
+        questionService.updatePromptById(QuestionTestValues.TEST_ID, QuestionTestValues.TEST_PROMPT);
+        String resultPrompt = questionService.findPromptById(QuestionTestValues.TEST_ID);
+        // then
+        Assertions.assertThat(resultPrompt).isEqualTo(QuestionTestValues.TEST_PROMPT);
+    }
+
+    @Test
+    public void shouldSelectPromptById() {
+        // given
+        SelectedEnglishKeywords selectedEnglishKeywords =
+                SelectedEnglishKeywords.builder()
+                        .difficulty(QuestionDefaultValues.DIFFICULTIES.get(0))
+                        .field(QuestionDefaultValues.FIELDS.get(0))
+                        .topic(QuestionDefaultValues.TOPICS.get(0))
+                        .build();
+
+        // when
+        questionService.selectPromptById(QuestionTestValues.TEST_ID, selectedEnglishKeywords);
+        String defaultPrompt = QuestionDefaultValues.TEMPLATE;
+        String expectedPrompt = generateExpectedPrompt(defaultPrompt);
+        // then
+        Assertions.assertThat(questionService.findPromptById(QuestionTestValues.TEST_ID)).isEqualTo(expectedPrompt);
+    }
+
+    private String generateExpectedPrompt(String defaultPrompt) {
+        return defaultPrompt.replace(QuestionDefaultValues.KEYWORD1, QuestionDefaultValues.DIFFICULTIES.get(0))
+                .replace(QuestionDefaultValues.KEYWORD2, QuestionDefaultValues.FIELDS.get(0))
+                .replace(QuestionDefaultValues.KEYWORD3, QuestionDefaultValues.TOPICS.get(0));
+    }
+}
